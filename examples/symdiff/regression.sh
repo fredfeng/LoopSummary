1cf9a3e58a90f00ea1ae89884869ca2239a7baef
Yu Feng <fengyu8299@gmail.com>
2019-10-25 17:46:15 -0700

    regression for symdiff

---
+++ examples/symdiff/regression.sh
@@ -0,0 +1,7 @@
+python3 check_eq.py benchmark1/C1.sol benchmark1/C2.sol
+python3 check_eq.py benchmark1/C1.sol benchmark1/C3.sol
+python3 check_eq.py benchmark2/c1.sol benchmark2/c2.sol
+python3 check_eq.py benchmark3/c1.sol benchmark3/c2.sol
+python3 check_eq.py benchmark4/c1.sol benchmark4/c2.sol
+python3 check_eq.py benchmark5/c1.sol benchmark5/c2.sol
+python3 check_eq.py benchmark6/c1.sol benchmark6/c2.sol
