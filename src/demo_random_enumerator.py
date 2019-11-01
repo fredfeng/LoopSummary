71f401e23885f6a91f3b40716aea7b548588c582
Yu Feng <fengyu8299@gmail.com>
2019-11-01 00:10:59 -0700

    sloppy enumerator/synthesizer

--- src/demo_random_enumerator.py
+++ src/demo_random_enumerator.py
@@ -10,24 +10,43 @@ from tyrell.logger import get_logger
 
 logger = get_logger('tyrell')
 
-toy_spec_str = '''
-enum SmallInt {
-  "0", "1", "2", "3"
-}
-value Int;
-value Empty;
 
-program Toy(Int, Int) -> Int;
-func const: Int -> SmallInt;
-func plus: Int -> Int, Int;
-func minus: Int -> Int, Int;
-func mult: Int -> Int, Int;
+#COPYRANGE(lockTime[_address], i0, lockNum[_address], tempLockTime, i0, lockNum[address], λ arg: arg+later-earlier)
+# for (uint i = 0; i < lockNum[_address]; ++i) {
+#   tempLockTime[_address][i] = lockTime[_address][i] + later-earlier;
+# }  
+
+toy_spec_str = '''
+enum startInt {
+  "0"
+}
+
+enum address {
+    "_address"
+}
+
+enum MapArray {
+    "lockTime", "tempLockTime"
+}
+
+enum MapInt {
+    "lockNum"
+}
+
+value Inst;
+value Stmt;
+value Empty;
+value endInt;
+value Array;
+
+program SymDiff(Stmt) -> Inst;
+func addressToArray: Array -> MapArray, address;
+func addressToInt: endInt -> MapInt, address;
+func COPYRANGE: Inst -> Array, startInt, endInt, Array, startInt, endInt;
 '''
 
 
-class ToyInterpreter(PostOrderInterpreter):
-    def eval_SmallInt(self, v):
-        return int(v)
+class SymDiffInterpreter(PostOrderInterpreter):
 
     def eval_const(self, node, args):
         return args[0]
@@ -40,6 +59,45 @@ class ToyInterpreter(PostOrderInterpreter):
 
     def eval_mult(self, node, args):
         return args[0] * args[1]
+    
+    def eval_addressToArray(self, node, args):
+        return args[0] + '[' + args[1] + ']'
+
+    def eval_addressToInt(self, node, args):
+        return args[0] + '[' + args[1] + ']'
+
+    def eval_COPYRANGE(self, node, args):
+        
+        src_array = args[0]
+        start_idx = args[1]
+        end_idx = args[2]
+        tgt_array = args[3]
+
+        loop_body = """
+            for (uint i = {tgtStart}; i < {tgtEnd}; ++i) {{
+                {tgtObj}[i] = {srcObj}[i];
+            }}
+        """.format(tgtStart=start_idx, tgtEnd=end_idx, tgtObj=tgt_array, srcObj=src_array)
+
+        contract_prog = """pragma solidity ^0.5.10;
+
+        contract C {{
+            
+            mapping (address => uint256[]) private {_storage1};
+            mapping (address => uint256[]) private {_storage2};
+            mapping (address => uint256) private {_storage3};
+            address private {_addr};
+
+            function foo() public {{
+
+                {_body}
+
+            }}
+        }}""".format(_body=loop_body, _storage1='lockTime', _storage2='tempLockTime', _storage3='lockNum', _addr='_address')
+
+        print(contract_prog)
+        # assert False
+        return "done"
 
 
 def execute(interpreter, prog, args):
@@ -63,13 +121,11 @@ def main(seed=None):
         enumerator=RandomEnumerator(
             spec, max_depth=4, seed=seed),
         decider=SymdiffDecider(
-            interpreter=ToyInterpreter(),
+            interpreter=SymDiffInterpreter(),
             examples=[
                 # we want to synthesize the program (x-y)*y (depth=3, loc=2)
                 # which is also equivalent to x*y-y*y (depth=3, loc=3)
                 Example(input=[4, 3], output=3),
-                Example(input=[6, 3], output=9),
-                Example(input=[1, 2], output=-2),
                 Example(input=[1, 1], output=0),
             ]
         )
