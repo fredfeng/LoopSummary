189e5fff24d24411e978905156b0dac7f0ab650d
Ben Mariano <benmariano13@gmail.com>
2020-04-06 09:10:24 -0500

    added init with houdini

--- src/tyrell/enumerator/__init__.py
+++ src/tyrell/enumerator/__init__.py
@@ -3,3 +3,4 @@ from tyrell.enumerator.smt import SmtEnumerator
 from tyrell.enumerator.random import RandomEnumerator
 from tyrell.enumerator.exhaustive import ExhaustiveEnumerator
 from tyrell.enumerator.from_iterator import FromIteratorEnumerator, make_empty_enumerator, make_singleton_enumerator, make_list_enumerator
+from tyrell.enumerator.houdini import HoudiniEnumerator
