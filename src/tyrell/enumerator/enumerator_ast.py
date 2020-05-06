d793d1c59fb768a22318e6c180f577bd42f6343f
Ben Mariano <benmariano13@gmail.com>
2020-05-06 14:18:15 -0500

    performance improvement for enumerator

--- src/tyrell/enumerator/enumerator_ast.py
+++ src/tyrell/enumerator/enumerator_ast.py
@@ -1,6 +1,8 @@
 from tyrell import spec as S
 import sys
 
+from collections import deque
+
 # sys.setrecursionlimit(1000000)
 
 class EnumeratorAST():
@@ -13,11 +15,14 @@ class EnumeratorAST():
         productions = self.builder.get_productions_with_lhs(typ)
         if len(productions) > 0:
             summarize = productions[0]            
-            self.queue = [FunctionNode(summarize, summarize.rhs, [],
-                                       func_deps, analysis, builder)]
+            # self.queue = [FunctionNode(summarize, summarize.rhs, [],
+            #                            func_deps, analysis, builder)]
+            self.queue = deque([FunctionNode(summarize, summarize.rhs, [],
+                                       func_deps, analysis, builder)])
         else:
             print("No candidates available!")
-            self.queue = []
+            # self.queue = []
+            self.queue = deque()
             
     def next_candidate(self):
         # If we had a finite grammar, we can run out of candidates
@@ -25,7 +30,8 @@ class EnumeratorAST():
             return None
 
         # Pop first node from queue
-        fnode = self.queue.pop(0)
+        # fnode = self.queue.pop(0)
+        fnode = self.queue.popleft()
 
         # If no more non-terminals, return candidate
         if fnode.complete():
@@ -36,11 +42,13 @@ class EnumeratorAST():
             # If no more options, back out
             if self.queue == []:
                 return None
-            fnode = self.queue.pop(0)
+            # fnode = self.queue.pop(0)
+            fnode = self.queue.popleft()
             
         # Expand first non-terminal in fnode and add to queue
         fnodes = fnode.expand()
-        self.queue += fnodes
+        # self.queue += fnodes
+        self.queue.extend(fnodes)
         
         # Since we didn't get a concrete candidate, try again
         return False
@@ -194,98 +202,98 @@ class FunctionNode():
         
         return r + "\n" + aggregate + "\n"
         
-class ProductionNode():
+# class ProductionNode():
 
-    def __init__(self, non_terms, terms):
-        self.non_terms = non_terms
-        self.terms = terms
+#     def __init__(self, non_terms, terms):
+#         self.non_terms = non_terms
+#         self.terms = terms
 
-    def build_candidate(self):
-        return None
+#     def build_candidate(self):
+#         return None
     
-class Node():
+# class Node():
 
-    def __init__(self, productions, builder):
-        if (len(productions) == 0):
-            raise Exception("No productions given!")
+#     def __init__(self, productions, builder):
+#         if (len(productions) == 0):
+#             raise Exception("No productions given!")
 
-        self.productions = productions
-        self.builder = builder
-        self.production_index = 0
-        self.candidate = self.build_candidate()
+#         self.productions = productions
+#         self.builder = builder
+#         self.production_index = 0
+#         self.candidate = self.build_candidate()
         
-    def build_candidate(self):
-        # Reset children from previous candidate
-        self.children = []
+#     def build_candidate(self):
+#         # Reset children from previous candidate
+#         self.children = []
 
-        # Return None if no more productions to choose from
-        if self.production_index >= len(self.productions):
-            return None
+#         # Return None if no more productions to choose from
+#         if self.production_index >= len(self.productions):
+#             return None
 
-        # Fetch next production
-        production = self.productions[self.production_index]
+#         # Fetch next production
+#         production = self.productions[self.production_index]
         
-        # Just build and return if it's a value
-        if not production.is_function():
-            return self.builder.make_node(production)            
+#         # Just build and return if it's a value
+#         if not production.is_function():
+#             return self.builder.make_node(production)            
 
-        # If a function, build each argument node and append to children
-        for arg_typ in production.rhs:
-            arg_node = Node(self.builder.get_productions_with_lhs(arg_typ), self.builder)
-            self.children.append(arg_node)
+#         # If a function, build each argument node and append to children
+#         for arg_typ in production.rhs:
+#             arg_node = Node(self.builder.get_productions_with_lhs(arg_typ), self.builder)
+#             self.children.append(arg_node)
 
-        # Return function candidate
-        return self.builder.make_node(production, self.get_children_candidates())
+#         # Return function candidate
+#         return self.builder.make_node(production, self.get_children_candidates())
 
-    def get_candidate(self):
-        return self.candidate
+#     def get_candidate(self):
+#         return self.candidate
 
-    def reset(self):
-        self.production_index = 0
-        self.candidate = self.build_candidate()
+#     def reset(self):
+#         self.production_index = 0
+#         self.candidate = self.build_candidate()
 
-    def get_children_candidates(self):
-        return list(map(lambda x: x.get_candidate(), self.children))
+#     def get_children_candidates(self):
+#         return list(map(lambda x: x.get_candidate(), self.children))
 
-    def next_candidate(self):
-        # Fetch current candidate
-        candidate = self.candidate
-        # Advance current candidate to next option
-        self.advance_candidate()
-        # Return current candidate
-        return candidate
+#     def next_candidate(self):
+#         # Fetch current candidate
+#         candidate = self.candidate
+#         # Advance current candidate to next option
+#         self.advance_candidate()
+#         # Return current candidate
+#         return candidate
         
-    def advance_candidate(self):        
-        # If current candidate is a value, simply fetch the
-        #   next candidate and build it
-        if self.children == []:
-            # Build next candidate
-            self.production_index += 1
-            self.candidate = self.build_candidate()
-            return
+#     def advance_candidate(self):        
+#         # If current candidate is a value, simply fetch the
+#         #   next candidate and build it
+#         if self.children == []:
+#             # Build next candidate
+#             self.production_index += 1
+#             self.candidate = self.build_candidate()
+#             return
             
-        # Increment first child by one. If it is out of options,
-        #   reset it and go on to next. Continue this until either
-        #   all are out of options, or one arg can be set to the next option
-        index = 0
-        while True:
-            new_child_cand = self.children[index].next_candidate()
-            if new_child_cand != None or index >= len(self.children):
-                break
-            self.children[index].reset()
-            index += 1
+#         # Increment first child by one. If it is out of options,
+#         #   reset it and go on to next. Continue this until either
+#         #   all are out of options, or one arg can be set to the next option
+#         index = 0
+#         while True:
+#             new_child_cand = self.children[index].next_candidate()
+#             if new_child_cand != None or index >= len(self.children):
+#                 break
+#             self.children[index].reset()
+#             index += 1
 
-        # If there were no options, this top level candidate is dead.
-        if self.children[index].get_candidate() == None:
-            # If this is the last candidate, there are no options
-            if self.production_index >= len(self.productions):
-                return None
-            # Otherwise, build a new candidate
-            self.production_index += 1
-            self.candidate = self.build_candidate()
-            return
+#         # If there were no options, this top level candidate is dead.
+#         if self.children[index].get_candidate() == None:
+#             # If this is the last candidate, there are no options
+#             if self.production_index >= len(self.productions):
+#                 return None
+#             # Otherwise, build a new candidate
+#             self.production_index += 1
+#             self.candidate = self.build_candidate()
+#             return
             
-        # Update with next iteration of this top-level candidate
-        self.candidate = self.build_candidate()
+#         # Update with next iteration of this top-level candidate
+#         self.candidate = self.build_candidate()
         
         
