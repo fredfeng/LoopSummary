05d5a028cf2dd581e69434ff01b2e48e9e5bd294
chyanju <chyanju@gmail.com>
2020-05-07 17:22:20 -0700

    Update creep.py

--- experiments/creep.py
+++ experiments/creep.py
@@ -12,6 +12,7 @@ def main(args):
     log_files = os.listdir(log_folder)
     n_solved = 0
     n_timeout = 0
+    n_exhausted = 0
     n_failed = 0
     for dfile in log_files:
         log_path = os.path.join(log_folder, dfile)
@@ -27,6 +28,9 @@ def main(args):
             if "Timeout." in raw_log:
                 n_timeout += 1
                 print("# {}, timeout.".format(dfile))
+            elif "Enumerator is exhausted" in raw_log:
+                n_exhausted += 1
+                print("# {}, exhausted.".format(dfile))
             else:
                 n_failed += 1
                 print("# {}, failed.".format(dfile))
@@ -34,6 +38,7 @@ def main(args):
     print("# solved       : {}".format(n_solved))
     print("# timeout      : {}".format(n_timeout))
     print("# failed       : {}".format(n_failed))
+    print("# exhausted    : {}".format(n_exhausted))
     print("# total        : {}".format(len(log_files)))
     print("# success rate : {:.4f}".format(n_solved/len(log_files)))
 
